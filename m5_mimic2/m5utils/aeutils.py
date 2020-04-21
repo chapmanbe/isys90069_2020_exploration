83b663468474b607850ca4850a5effde028a9ed0
Brian Chapman <chapmanbe@gmail.com>
2020-04-21 20:18:05 +1000

    a currently working version

---
+++ m5_mimic2/m5utils/aeutils.py
@@ -0,0 +1,281 @@
+from IPython.display import clear_output, display, HTML
+import ipywidgets as ipw
+import numpy as np
+import warnings
+import io
+import os
+import pyarrow as pa
+import pandas as pd
+import altair as alt
+from collections import defaultdict
+from . description import drg, table_text
+from markdown import markdown
+
+
+DATADIR = "/home/shared/mimic_data/case_level"
+GRAPH_WIDTH=800
+GRAPH_HEIGHT=300
+global_debugs = {}
+cases = (21372, 14125, 22498, 10299,
+         2284, 15239, 4727, 8292, 9969,
+         17805, 1766, 29839, 29657, 11401,
+         19754, 4785, 19847, 6973, 27421, 18514)
+
+
+debug_out = ipw.Output()
+_debug = ipw.Label(value="")
+def du(msg):
+    _debug.value = _debug.value + " " +msg
+
+def read_data():
+    def concat_data(case_data):
+        dw = defaultdict(list)
+        for k,v in case_data.items():
+            for kk, vv in v.items():
+                dw[kk].append(vv)
+        return {k:pd.concat(v) for k,v in dw.items()}
+    case_data = {}
+    fails = []
+    for c in cases:
+        with open(os.path.join(DATADIR,"%d.feather"%c), "rb") as f0:
+            try:
+                case_data[c] = pa.deserialize(f0.read())
+            except pa.ArrowInvalid:
+                fails.append(c)
+    return concat_data(case_data)
+
+def get_case_options(data, table=None):
+    if table == None:
+        table = list(data.keys())[0]
+    options = list(data[table].subject_id.unique())
+    options.sort()
+    return options
+
+def get_table_options(data):
+
+    table_options = list(data.keys())
+    table_options.sort()
+    return table_options
+
+def get_column_options(data):
+    column_options = {t:list(data[t].columns) for t in data.keys()}
+    for k,v in column_options.items():
+        v.sort()
+    return column_options
+
+def plt_nominal(df, vizvar):
+    du("plt_nominal")
+    tmp = {k:df[k].value_counts().to_frame().reset_index().rename(columns={"index":vizvar, k:"counts"}) for k in df.keys()}
+
+    for k,v in tmp.items():
+        v["subject"] = k
+    tmp = pd.concat(tmp.values(), axis=0, sort=True).fillna(0)
+
+    c = alt.Chart(tmp).mark_bar().encode(
+            x='sum(counts):Q',
+            y=alt.X('%s:N'%vizvar, axis=alt.Axis(labelAngle=45)),
+            color=alt.Color('subject:N')
+    ).properties(width=GRAPH_WIDTH,
+                        height=GRAPH_HEIGHT)
+    return c
+
+
+def plt_numeric(df, vizvar):
+    du("plt_numeric")
+
+    c0 = alt.Chart(df.dropna()).transform_fold(
+        list(df.columns),
+        as_=['Subject', vizvar]
+    ).transform_filter(
+        'isValid(datum.Subject)'
+    ).transform_density(
+        vizvar,
+        groupby=['Subject'],
+        as_=[vizvar, 'density'],
+        counts=False
+    ).mark_area(
+        opacity=0.3
+    ).encode(
+        x='%s:Q'%vizvar,
+        y='density:Q',
+        color=alt.Color('Subject:N')
+    )
+    return c0
+
+def tidy_data(data, cases, dfilter, dfilter_value, pop):
+    du(str(cases))
+    tmp = data[data["subject_id"].isin(cases)]
+    du(str(tmp.shape))
+    if dfilter_value :
+        du("filer by value")
+        tmp = tmp[tmp[dfilter]==dfilter_value]
+
+    return pd.DataFrame.from_dict({k:tmp[tmp.subject_id==k][pop].to_list() for k in cases}, orient='index').T.rename(columns={k:str(k) for k in cases})
+
+def get_plts():
+    du("get_plts")
+
+    df = tidy_data(all_data[table_select.value], case_select.value, column_select.value, filter_select.value, viz_select.value)
+    du("got tidy")
+    _debug.value = ""
+    global_debugs["get_plts_df"] = df.copy()
+    du(str(df.dtypes))
+    if df.dtypes[0] in {np.dtype("float64")}:
+        c1 = plt_numeric(df, viz_select.value)
+    else:
+        c1 = plt_nominal(df, viz_select.value)
+    return c1
+
+def draw_plot(c):
+    du("draw_plot")
+    with io.StringIO() as f:
+        c.save(f, format="html")
+        f.seek(0)
+        html1 = f.read()
+    with plots_out1:
+        display(HTML(html1))
+
+def plt_data():
+    du("plt_data\n")
+    c = get_plts()
+    draw_plot(c)
+
+def view_table():
+    du("view_table")
+    df = all_data[table_select.value]
+    df= df[df.subject_id.isin(case_select.value)]
+    vs = slice_select.value
+    with table_out:
+        clear_output()
+        display(df[vs:vs+5])
+
+def comp_view_max():
+    du("comp_view_max")
+    df = all_data[table_select.value]
+    return max(0,df[df.subject_id.isin(case_select.value)].shape[0]-5)
+
+def update_view_max():
+    slice_select.max = comp_view_max()
+
+def update_table(change):
+    #du("update_table")
+    table = change.new
+    update_case(change)
+    #view_table()
+    #plt_data()
+
+def update_case(change):
+    #du("update_case")
+    update_view_max()
+    column_select.options = column_options[table_select.value]
+    du("updated column options")
+    view_table()
+    update_column(change)
+
+
+def set_columns():
+    #du("set_columns")
+    df = all_data[table_select.value]
+    df = df[df.subject_id.isin(case_select.value)]
+    col = column_select.value
+    cslabels = [("%s: %s"%(i[0],i[1]), i[0]) for i in  df[col].value_counts().iteritems()]
+    cslabels.sort()
+
+    # Update downstream choices
+    viz_select.options = column_options[table_select.value]
+    cslabels.insert(0, ("None",""))
+    filter_select.options = cslabels
+
+def update_column(change):
+    #du("update_column")
+    set_columns()
+
+    plt_data()
+
+def update_filter(change):
+    plt_data()
+
+def update_viz(change):
+    plt_data()
+
+def update_slice(change):
+    view_table()
+
+def init_view():
+    case_select.value = [case_options[0]]
+    table_select.value = table_options[0]
+    column_select.options = column_options[table_select.value]
+    set_columns()
+    view_table()
+    plt_data()
+
+def on_reset(b):
+    init_view()
+
+## Readin Data
+
+all_data = read_data()
+table_options = get_table_options(all_data)
+case_options = get_case_options(all_data)
+column_options = get_column_options(all_data)
+
+## Define Widgets
+
+table_out = ipw.Output()
+counts_out = ipw.Output()
+plots_out1 = ipw.Output(layout=ipw.Layout(width="100%", min_height="%dpix"%(GRAPH_HEIGHT+50),
+                                                        max_height="%dpix"%(GRAPH_HEIGHT+100),
+                                         border="solid", align="center"))
+
+case_select = ipw.SelectMultiple(options = case_options, value=[case_options[0]])
+table_select = ipw.Dropdown(options = table_options)
+column_select = ipw.Dropdown(options = column_options[table_select.value])
+slice_select = ipw.IntSlider(min=0, layout=ipw.Layout(width="100%"))
+
+filter_select = ipw.Dropdown()
+viz_select = ipw.Dropdown()
+reset = ipw.Button(decription="Reset", layout=ipw.Layout(width="100%", align="center"))#,  button_style='danger')
+reset.style.button_color = 'lightgreen'
+
+### Define label widgets
+
+cw = ipw.Label(value="Select case")
+tw = ipw.Label(value="Select table")
+fw = ipw.Label(value="Select filter variable")
+vw = ipw.Label(value="Select filter value")
+pw = ipw.Label(value="Select plot variable")
+
+## Set call backs
+table_select.observe(update_table, names='value', type='change')
+case_select.observe(update_case, names="value", type="change")
+column_select.observe(update_column, names="value", type="change")
+filter_select.observe(update_filter, names="value", type="change")
+viz_select.observe(update_viz, names="value", type="change")
+slice_select.observe(update_slice, names="value", type="change")
+reset.on_click(on_reset)
+
+aexplore_mimic = ipw.TwoByTwoLayout(
+        top_left=ipw.VBox([reset,
+                           ipw.HBox([ipw.VBox([cw, case_select]),
+                                     ipw.VBox([tw, table_select]),
+                                     ipw.VBox([fw, column_select]),
+                                     ipw.VBox([vw, filter_select]),
+                                     ipw.VBox([pw, viz_select])]),
+                           slice_select,
+                           table_out],
+                           layout=ipw.Layout(width='100%',
+                                             min_height='200px',
+                                             border="solid")),
+        bottom_left=ipw.HBox([plots_out1]), layout=ipw.Layout(width="100%"))
+
+debug_out = ipw.Output()
+_debug = ipw.Label(value="")
+
+descriptions = ipw.Tab()
+children = [ipw.HTML(value=markdown(table_text[t])) for t in table_options]
+children.append(ipw.HTML(drg))
+tmp = table_options[:]
+tmp.append("DRG")
+for i in range(len(tmp)):
+    descriptions.set_title(i,tmp[i])
+descriptions.children = children
